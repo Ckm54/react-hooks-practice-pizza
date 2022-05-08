4b13a5ac77c29d883edf8621eb5f1602c5124ac5
Collins <mutaic84@gmail.com>
2022-05-08 10:24:34 +0300

    create handler for edit button click

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-function Pizza({pizza}) {
+function Pizza({pizza, onEditPizza}) {
+  function handleButtonClick() {
+    onEditPizza(pizza)
+  }
   return (
     <tr>
       <td>{pizza.topping}</td>
       <td>{pizza.size}</td>
       <td>{pizza.vegetarian ? "yes" : "no"}</td>
       <td>
-        <button type="button" className="btn btn-primary">
+        <button type="button" className="btn btn-primary" onClick={handleButtonClick}>
           Edit Pizza
         </button>
       </td>
