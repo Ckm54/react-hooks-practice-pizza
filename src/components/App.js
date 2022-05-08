b0dc62a8f932d725bb36d8518d08547e6f73cc70
Collins <mutaic84@gmail.com>
2022-05-08 10:17:55 +0300

    fetch pizza data

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import PizzaForm from "./PizzaForm";
 import PizzaList from "./PizzaList";
 
 function App() {
+  const [pizzaData, setPizzaData] = useState([])
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/pizzas`)
+    .then((res) => res.json())
+    .then((data) => setPizzaData(data))
+  }, []);
+
   return (
     <>
       <Header />
       <PizzaForm />
-      <PizzaList />
+      <PizzaList pizzas={pizzaData}/>
     </>
   );
 }
